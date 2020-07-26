4f5da617c1a5ff24efaeed2b4bd4a7b8f9f1fda8
HackYourFuture <hackyourfuture@HackYouturesAir.fritz.box>
2020-07-26 19:01:32 +0200

    created files, finished markup and style

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const celsiusInput = document.querySelector('#celsius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+celsiusInput.addEventListener('input', function() {
+    console.log('hello');
+    console.log('hi'); 
+})
